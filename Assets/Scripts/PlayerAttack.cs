51290ad0828187a8ff37476ca08ce8e4afaf5edb
grantmccord <gmccord6502@comcast.net>
2023-03-11 23:58:18 -0500

    Organized everything and added a semi functional attack animation

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -11,9 +11,12 @@ public class PlayerAttack : MonoBehaviour
     private float timeToAttack = 0.25f;
     private float timer = 0f;
 
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         attackArea = transform.GetChild(0).gameObject;
     }
 
@@ -27,12 +30,17 @@ public class PlayerAttack : MonoBehaviour
 
         if (attacking)
         {
+            if (!anim.GetBool("isAttacking"))
+            {
+                anim.SetBool("isAttacking", true);
+            }
             timer += Time.deltaTime;
 
             if (timer >= timeToAttack)
             {
                 timer = 0;
                 attacking = false;
+                anim.SetBool("isAttacking", false);
                 attackArea.SetActive(attacking);
             }
         }
